a9154a91bdd7446d899dc96aa792e6f83199f321
alexandre sobrinho <linuxman.df@gmail.com>
2021-05-27 20:29:01 -0300

    DTO e user resource

--- src/main/java/com/workshopmongo/resources/UserResource.java
+++ src/main/java/com/workshopmongo/resources/UserResource.java
@@ -1,8 +1,7 @@
 package com.workshopmongo.resources;
 
-import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
+import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
@@ -11,6 +10,7 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.workshopmongo.domain.User;
+import com.workshopmongo.dto.UserDTO;
 import com.workshopmongo.services.UserService;
 
 @RestController
@@ -21,11 +21,12 @@ public class UserResource {
 	 private UserService service;
 	
 	@RequestMapping(method=RequestMethod.GET)
-	public ResponseEntity<List<User>> findAll(){
+	public ResponseEntity<List<UserDTO>> findAll(){
 		
 		List <User> list = service.findAll();
+		List<UserDTO> listDTO =  list.stream().map(x -> new UserDTO(x)).collect(Collectors.toList());
 		//list.addAll(Arrays.asList(maria, alex));
-		return  ResponseEntity.ok().body(list);
+		return  ResponseEntity.ok().body(listDTO);
 		
 	}
 
