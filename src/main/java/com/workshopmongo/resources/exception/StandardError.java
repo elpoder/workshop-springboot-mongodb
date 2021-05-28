bf81b19b8295d55bf0c98690397ce6d0b1a280d6
alexandre sobrinho <linuxman.df@gmail.com>
2021-05-27 21:07:54 -0300

    findByID implementado

---
+++ src/main/java/com/workshopmongo/resources/exception/StandardError.java
@@ -0,0 +1,5 @@
+package com.workshopmongo.resources.exception;
+
+public class StandardError {
+
+}
--- src/main/java/com/workshopmongo/services/UserService.java
+++ src/main/java/com/workshopmongo/services/UserService.java
@@ -1,12 +1,14 @@
 package com.workshopmongo.services;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.workshopmongo.domain.User;
 import com.workshopmongo.repository.UserRepository;
+import com.workshopmongo.services.exception.ObjectNotFoundException;
 
 @Service
 public class UserService {
@@ -16,6 +18,17 @@ public class UserService {
 	
 	public List<User> findAll(){
 		return repo.findAll();
+	}
+	
+	public User findById(String id) {
+		Optional<User> user = repo.findById(id);
+		
+		if (user == null) {
+			throw new ObjectNotFoundException("Usuário não encontrado");
+		}
+		else {
+			return user.get();
+		}
 	}
 
 }
